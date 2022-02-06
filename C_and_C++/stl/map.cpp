17633c860fc4615e089f9dc05a89ff1f379de6b2
Rohit Singh <rohitsinghemail@protonmail.com>
2022-02-06 21:48:23 +0530

    added readmw

--- C_and_C++/stl/map.cpp
+++ C_and_C++/stl/map.cpp
@@ -8,6 +8,14 @@ void printMap(map<int, string> &m){
     }
 }
 
+void printUnorderedMap(unordered_map<int, string> &m){
+    cout<<"Unordered Map: "<<endl;
+    cout<<"Size is: "<<m.size()<<endl;
+    for(auto p : m){
+        cout<<"Key is "<<p.first<<" value is "<<p.second<<endl;
+    }
+}
+
 int main(){
     
 
@@ -32,19 +40,28 @@ int main(){
     m.clear();
     printMap(m);
 
-    map<string, int> l;
+    // map<string, int> l;
+    // cout<<"Enter the no of strings: "<<endl;
+    // int n;
+    // cin>>n;
+    // for(int i = 0; i< n; i++){
+    //     string s;
+    //     cin>>s;
+    //     l[s] = l[s] + 1;
+    // }
+    // for(pair<string, int> st : l){
+    //     cout<<st.first<< " "<<st.second<<endl;
+    // }
 
-    cout<<"Enter the no of strings: "<<endl;
-    int n;
-    cin>>n;
-    for(int i = 0; i< n; i++){
-        string s;
-        cin>>s;
-        l[s] = l[s] + 1;
-    }
-    for(pair<string, int> st : l){
-        cout<<st.first<< " "<<st.second<<endl;
-    }
+    unordered_map<int, string> l1;
+    l1[1] = "abc";
+    l1[5] = "cdc";
+    l1[3] = "acd";
+    l1[6] = "a";
+    l1[5] = "cde";
+
+    printUnorderedMap(l1);
+
 
     return 0;
 }
