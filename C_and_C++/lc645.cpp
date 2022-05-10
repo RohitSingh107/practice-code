95f13c5215b53e0b455eb12155ae7124ea7f33d5
Rohit Singh <RohitSinghEmail@protonmail.com>
2022-05-10 22:32:43 +0530

    new code

--- C_and_C++/lc645.cpp
+++ C_and_C++/lc645.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
@@ -9,21 +10,60 @@ using namespace std;
 
 vector<int> findErrorNums(vector<int>& nums) {
 	int n = nums.size();
-	vector<int> ans;
+	vector<int> ans(2);
 
 	int tmp = 0;
 	for(auto i : nums){
 		tmp ^= i;
 	}
-	tmp = 4;
-	int tmp2 = tmp;
-	int i = 0;
-	while(!(tmp & 1)){
-		tmp2 =tmp2>>1;
-		i++;
+	for(int i = 1; i<= n; i++){
+		tmp ^= i;
 	}
 
-	cout<<i<<endl;
+
+	int tmp2 = tmp;
+	int j = 0;
+	while(!(tmp2 & 1)){
+		/* std::cout << tmp2 << std::endl; */
+		tmp2 =tmp2>>1;
+		j++;
+	}
+	int buck1 = 0;
+	int buck2 = 0;
+	for(auto i : nums){
+		if(i & (1<<j)){
+			/* std::cout << "Odd: "<<i << std::endl; */
+			buck1 ^= i;
+			/* std::cout << std::endl; */
+		}
+		else{
+			/* std::cout << "Even: "<<i << std::endl; */
+			buck2 ^= i;
+			/* std::cout << std::endl; */
+		}
+	}
+
+	/* cout<<tmp<<endl; */
+
+	/* std::cout << buck1 <<" "<<buck2<< std::endl; */
+
+	int p1 = 0;
+	int p2 = 0;
+	for(int i = 1; i<=n; i++){
+		if(i & (1<<j)){
+			/* std::cout << "Odd: "<<i << std::endl; */
+			p1 ^= i;
+			/* std::cout << std::endl; */
+		}
+		else{
+			/* std::cout << "Even: "<<i << std::endl; */
+			p2 ^= i;
+			/* std::cout << std::endl; */
+		}
+	}
+	ans[1] = buck1 ^ p1;
+	ans[0] = tmp ^ ans[1];
+
 	return ans;
 }
 
@@ -31,7 +71,10 @@ int32_t main(){
 	
 	vector<int> nums= {1,2,2,4};
 
-	findErrorNums(nums);
+	for(auto i : findErrorNums(nums)){
+		std::cout << i << std::endl;
+	}
+	/* findErrorNums(nums); */
 
 	return 0;
 }
