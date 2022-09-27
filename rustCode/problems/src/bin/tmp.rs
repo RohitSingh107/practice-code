9b667bc2cc77b5e44dd17e100c58638cd27dd7f4
Rohit <RohitSinghEmail@protonmail.com>
2022-09-27 17:24:54 +0530

    new code

--- rustCode/problems/src/bin/tmp.rs
+++ rustCode/problems/src/bin/tmp.rs
@@ -1,79 +1,42 @@
-// https://leetcode.com/problems/partition-equal-subset-sum/
+use proconio::input;
+use std::cmp::max;
 
-fn f(i: usize, target: i32, nums: &Vec<i32>, dp: &mut Vec<Vec<i32>>) -> bool {
-    if target == 0 {
-        return true;
+fn knapsack(i: usize, wl: i64, items: &Vec<(i64, i64)>, dp: &mut Vec<Vec<i64>>) -> i64 {
+
+    if wl <= 0 {
+        // println!("i is {}, wl is {}", i, wl);
+        return -1 * 1000000000;
     }
-
-    if target < 0 {
-        return false;
-    }
-
+    // println!("i is {}, wl is {}", i, wl);
     if i == 0 {
-        return nums[i] == target;
-    }
-
-    if dp[i][target as usize] != -1 {
-        if dp[i][target as usize] == 1 {
-            return true;
+        if wl >= items[0].0 {
+            println!("i is {}, wl is {}", i, wl);
+            return items[0].1;
         }
-        return false;
+
+        println!("i is {}, wl is {}", i, wl);
+        return 0;
     }
 
-    let ans = f(i - 1, target, nums, dp) || f(i - 1, target - nums[i], nums, dp);
-    if ans == true {
-        dp[i][target as usize] = 1;
-        return true;
+    if wl <= 0 {
+        // println!("i is {}, wl is {}", i, wl);
+        return -1 * 1000000000;
     }
 
-    dp[i][target as usize] = 0;
-    return false;
-}
-
-fn can_partition(nums: Vec<i32>) -> bool {
-    let n = nums.len();
-
-    let k = 12;
-
-    // // TopDown Recursion
-    // let mut dp: Vec<Vec<i32>> = vec![vec![-1; k as usize + 1]; n + 1];
-    // f(n - 1, k / 2, &nums, &mut dp)
-
-    // BottomUp Tabulation
-
-    let mut dp: Vec<Vec<bool>> = vec![vec![false; k as usize + 1]; n + 1];
-
-    for i in 0..n {
-        for t in 0..(k as usize + 1) {
-            if t == 0 {
-                dp[i][t] = true;
-                continue;
-            }
-
-            if i == 0 {
-                dp[i][t] = nums[i] as usize == t;
-                continue;
-            }
-
-            dp[i][t] = dp[i - 1][t];
-            if t < nums[i] as usize {
-                continue;
-            }
-            dp[i][t] = dp[i][t] || dp[i - 1][t - nums[i] as usize];
-        }
-    }
-
-    for i in 0..n {
-        for j in 0..(k as usize + 1) {
-            print!("{}\t", dp[i][j]);
-        }
-        print!("\n");
-    }
-
-    return dp[n - 1][k as usize];
+    let not_take = knapsack(i - 1, wl, items, dp) + 0;
+    let take = knapsack(i - 1, wl - items[i].0, items, dp) + items[i].1;
+    let tmp = max( take, not_take);
+    println!("i is {}, wl is {}, take is {}, not_take is {}", i, wl, take, not_take);
+    return tmp;
 }
 
 fn main() {
-    let nums = vec![3,2,7];
-    println!("{}", can_partition(nums));
+    input! {
+        n : usize,
+        w : usize,
+        items : [(i64, i64) ; n],
+    }
+
+    let mut dp: Vec<Vec<i64>> = vec![vec![-1; w + 1]; n + 1];
+    println!("{}", knapsack(n - 1, w as i64, &items, &mut dp));
 }
