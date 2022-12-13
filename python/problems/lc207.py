222310d48eca766a78a2c6f1b2d79db04564fa89
Rohit <RohitSinghEmail@protonmail.com>
2022-12-13 18:51:36 +0530

    new code

--- python/problems/lc207.py
+++ python/problems/lc207.py
@@ -1,10 +1,39 @@
 # https://leetcode.com/problems/course-schedule/
 
-def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-    pass
+from typing import List, Deque
+from collections import deque
+
+def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+    graph : List[List[int]] = [[] for _ in range(numCourses)]
+    indegree = [0 for _ in range(numCourses)]
+
+    for p in prerequisites:
+        graph[p[1]].append(p[0])
+        indegree[p[0]] += 1
+
+
+    q : Deque[int] = deque([n for n in range(numCourses) if indegree[n] == 0])
+    result = []
+
+    while len(q) != 0:
+        v = q.popleft()
+
+        for n in graph[v]:
+            indegree[n] -= 1
+            if indegree[n] == 0:
+                q.append(n)
+
+        result.append(v)
+
+    print(result)
+
+    return bool(len(result) == numCourses)
 
 def main():
-    pass
+    numCourses = 2
+    prerequisites = [[1,0]]
+    print(canFinish(numCourses, prerequisites))
 
 if __name__ == "__main__":
     main()
