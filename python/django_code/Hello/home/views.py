1ee62f33b8f975bc24ceb8a89cde342b1582f1fc
Rohit <RohitSinghEmail@protonmail.com>
2022-12-22 17:02:08 +0530

    new code

--- python/django_code/Hello/home/views.py
+++ python/django_code/Hello/home/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+
 def index(request):
-    context = {
-            'variable1': "tgis is variale from python",
-            'variable2': "tgis is 2nd variale from python"
-            }
-    return render(request, 'index.html', context)
+    # context = {
+    #         'variable1': "tgis is variale from python",
+    #         'variable2': "tgis is 2nd variale from python"
+    #         }
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, 'about.html')
+
 
 def contact(request):
-    return HttpResponse("This is contact page")
+    return render(request, 'contact.html')
+
 
 def services(request):
-    return HttpResponse("This is srvices pagee")
+    return render(request, 'services.html')
