8c38b35ddb40c7b97d62abf0d16e9deef615ddd5
Rohit <RohitSinghEmail@protonmail.com>
2022-12-22 23:22:07 +0530

    django

--- python/django_code/Hello/home/models.py
+++ python/django_code/Hello/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+# makemigrations - Create changes and store in a file
+# migrate - apply the pending changes created by makemigrations
+
 # Create your models here.
+class Contact(models.Model):
+    """Contact model"""
+    name = models.CharField(max_length=122)
+    email = models.EmailField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+    # date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+        """docstring for __str_"""
+        
--- python/django_code/Hello/home/views.py
+++ python/django_code/Hello/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.core.handlers.wsgi import WSGIRequest
+from django.contrib import messages
 
-
-def index(request):
+def index(request: WSGIRequest):
+    print(f"type of request is {type(request)}")
+    print(request)
+    # messages.success(request, "This is test message")
     # context = {
     #         'variable1': "tgis is variale from python",
     #         'variable2': "tgis is 2nd variale from python"
@@ -10,13 +16,27 @@ def index(request):
     return render(request, 'index.html')
 
 
-def about(request):
+def about(request: WSGIRequest):
     return render(request, 'about.html')
 
 
-def contact(request):
+def contact(request: WSGIRequest):
+    print(f"type of request is {type(request)}")
+    print(request)
+    if request.method == 'POST':
+        name =  request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc =  request.POST.get('desc')
+        contact : Contact = Contact(name = name, email = email, phone = phone, desc = desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+        
+
+    
+
     return render(request, 'contact.html')
 
 
-def services(request):
+def services(request: WSGIRequest):
     return render(request, 'services.html')
