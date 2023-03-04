5490b80a8f78397437d238d350101f7b234391df
Rohit <RohitSinghEmail@protonmail.com>
2023-03-04 19:58:01 +0530

    code to newpipe to toutub

---
+++ python/youtube/newpipe2youtube/main.py
@@ -0,0 +1,72 @@
+# from apiclient.discovery import build
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+# youtube = build('youtube', 'v3', developerKey = api_key)
+
+# # print(type(youtube))
+
+# req = youtube.search().list(q = 'avengers', part = 'snippet', type = 'video')
+
+# print(type(req))
+
+# res = req.execute()
+
+# # print(res)
+
+
+# print("playlist")
+
+# req = youtube.playlists().insert(part = 'snippet', onBehalfOfContentOwner= 'testplaylist')
+
+# res = req.execute()
+
+# print(res)
+
+
+
+scopes = ["https://www.googleapis.com/auth/youtube"]
+
+
+client_secrets_file = "cs.json"
+
+
+print("here1------------------------------------------")
+# Get credentials and create an API client
+flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+flow.run_local_server()
+credentials = flow.credentials
+
+youtube = build("youtube", "v3", credentials=credentials)
+print("here1------------------------------------------")
+
+# request = youtube.playlists().insert(
+#     part="snippet",
+#     body={
+#         "snippet": {
+#             "title": "test playlist",
+#             "description": "This is a test playlist"
+#         }
+#     }
+# )
+
+
+vid_ids = ["gyMwXuJrbJQ", "pjSxOnCkHIA", "Y2Oj9gllHno"]
+
+for vid_id in vid_ids:
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId" :"PLfLReqs4zJODuyADQ8NE6w_AZLC5DJNZO",
+            
+                "resourceId" : {
+                    "kind" : "youtube#video",
+                    "videoId" : vid_id,
+                    },
+            }
+        }
+    )
+    response = request.execute()
+    print(response)
