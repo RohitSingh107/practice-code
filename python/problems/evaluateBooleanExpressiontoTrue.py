7377f09e87b735d11c4783c28b6878efc734b3fb
Rohit <RohitSinghEmail@protonmail.com>
2023-06-06 22:12:30 +0530

    new code

--- python/problems/evaluateBooleanExpressiontoTrue.py
+++ python/problems/evaluateBooleanExpressiontoTrue.py
@@ -52,7 +52,7 @@ def f(i, j, isTrue, s, dp) -> int:
             else:
                 ans += lT * rT + lF * rF
 
-    dp[i][j][isTrue] = ans
+    dp[i][j][isTrue] = ans % 1003
 
 
     return dp[i][j][isTrue]
@@ -67,8 +67,59 @@ class Solution:
 
         l = len(A)
 
-        dp = [[[-1 for _ in range(2)] for _ in range(l+1)] for _ in range(l+1)]
-        return f(0, l -1, True, A, dp)
+        # dp = [[[-1 for _ in range(2)] for _ in range(l+1)] for _ in range(l+1)]
+        # return f(0, l -1, True, A, dp)
+        dp = [[[0 for _ in range(2)] for _ in range(l+1)] for _ in range(l+1)]
+        
+        for i in range(l-1, 0 -1, -1):
+            for j in range(i, l):
+
+                for b in range(2):
+                    if i > j:
+                        dp[i][j][b] = 0
+
+                    # print(f"i is {i}, j is {j}")
+                    if i == j:
+                        if b:
+                            # print(f"i is {i}, j is {j}")
+                            dp[i][j][b] = int(A[i] == 'T')
+                        else:
+                            dp[i][j][b] = int(A[i] == 'F')
+
+                        continue
+
+                    
+                    ans = 0
+                    # print("here1------------------------------")
+
+                    for k in range(i+1, j, 2):
+
+                        # print(f"i is {i}, j is {j}, k is {k}")
+                        lT = dp[i][k-1][1]
+                        lF = dp[i][k-1][0]
+                        rT = dp[k+1][j][1]
+                        rF = dp[k+1][j][0]
+
+                        if A[k] == '|':
+                            if b:
+                                ans += lT * rT + lT * rF + lF * rT
+                            else:
+                                ans += lF * rF
+                        elif A[k] == '&':
+                            if b:
+                                ans += lT * rT
+                            else:
+                                ans += lT * rF + lF * rT + lF * rF
+                        else:
+                            if b:
+                                ans += lT * rF + lF * rT
+                            else:
+                                ans += lT * rT + lF * rF
+
+                    dp[i][j][b] = ans % 1003
+        # print(dp)
+        return dp[0][l-1][1]
+
 
 
 def main():
