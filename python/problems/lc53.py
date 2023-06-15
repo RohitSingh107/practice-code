7854463663cb1a261e9561077f702bb213666dff
Rohit <RohitSinghEmail@protonmail.com>
2023-06-15 15:33:55 +0530

    new code

---
+++ python/problems/lc53.py
@@ -0,0 +1,38 @@
+
+
+
+from collections import deque
+from typing import List, Deque
+
+import sys
+
+
+
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        ans = -sys.maxsize -1
+
+        curr = 0
+        for i in nums:
+            curr += i
+            if curr < 0:
+                curr = 0
+            ans = max(ans, curr)
+            # print(f"curr is curr {curr}, i is {i}")
+        return ans
+
+
+
+
+
+
+def main():
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    solution = Solution()
+    print(f"ans is {solution.maxSubArray(nums=nums)}")
+
+if __name__ == '__main__':
+    main()
