235debd1116bd4066ebc5a3f6f3a3bc0f9177911
Rohit <RohitSinghEmail@protonmail.com>
2024-03-19 22:17:33 +0530

    all code push

--- python/scrapy_demo/bookscraper/bookscraper/settings.py
+++ python/scrapy_demo/bookscraper/bookscraper/settings.py
@@ -14,7 +14,10 @@ BOT_NAME = "bookscraper"
 SPIDER_MODULES = ["bookscraper.spiders"]
 NEWSPIDER_MODULE = "bookscraper.spiders"
 
-FEEDS = {"booksdata.json": {"format": "json", 'overwrite': True}, "booksdata.csv": {"format": "csv", 'overwrite': True}}
+FEEDS = {
+    "booksdata.json": {"format": "json", "overwrite": True},
+    "booksdata.csv": {"format": "csv", "overwrite": True},
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = "bookscraper (+http://www.yourdomain.com)"
@@ -54,9 +57,11 @@ ROBOTSTXT_OBEY = True
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-   # "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
-   # 'bookscraper.middlewares.ScrapeOpsFakeUserAgentMiddleware' : 400, 
-   'bookscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware' : 400, 
+    # "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
+    # 'bookscraper.middlewares.ScrapeOpsFakeUserAgentMiddleware' : 400,
+    "bookscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware": 400,
+    # "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
+    # "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
 }
 
 # Enable or disable extensions
@@ -104,5 +109,9 @@ SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = "https://headers.scrapeops.io/v1/user-agent
 SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
 SCRAPEOPS_NUM_RESULTS = 50
 
-SCRAPEOPS_FAKE_BROWSER_HEADER_ENDPOINT = "https://headers.scrapeops.io/v1/browser-headers"
+SCRAPEOPS_FAKE_BROWSER_HEADER_ENDPOINT = (
+    "https://headers.scrapeops.io/v1/browser-headers"
+)
 SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED = True
+
+ROTATING_PROXY_LIST = ["197.234.13.45:4145", "94.26.108.67:2580", "202.4.107.69:5678"]
