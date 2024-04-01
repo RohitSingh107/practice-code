7d93bc0abac208f78233eff26bab47864293f04f
Rohit Singh <RohitSinghEmail@protonmail.com>
2024-04-01 22:28:00 +0530

    shell changes

--- python/flet_learning/flet_android_app/flake.nix
+++ python/flet_learning/flet_android_app/flake.nix
@@ -1,5 +1,3 @@
-
-
 {
   description = "moxplatform";
   inputs = {
@@ -9,7 +7,13 @@
     android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, android-nixpkgs }: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    android-nixpkgs,
+  }: let
+    system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       config = {
@@ -18,91 +22,104 @@
       };
     };
     # Everything to make Flutter happy
-    sdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
-      cmdline-tools-latest
-      build-tools-30-0-3
-      build-tools-33-0-2
-      build-tools-34-0-0
-      platform-tools
-      emulator
-      # patcher-v4
-      platforms-android-30
-      platforms-android-31
-      platforms-android-33
-      platforms-android-34
+    sdk = android-nixpkgs.sdk.${system} (sdkPkgs:
+      with sdkPkgs; [
+        cmdline-tools-latest
+        build-tools-30-0-3
+        build-tools-33-0-2
+        build-tools-34-0-0
+        platform-tools
+        emulator
+        # patcher-v4
+        platforms-android-30
+        platforms-android-31
+        platforms-android-33
+        platforms-android-34
 
-      ndk-23-1-7779620 
-      cmake-3-18-1
-    ]);
+        ndk-23-1-7779620
+        cmake-3-18-1
+      ]);
     pinnedJDK = pkgs.jdk17;
   in {
-    devShell = pkgs.mkShell rec {
-      buildInputs = with pkgs; [
-        # Android
-        pinnedJDK
-        sdk
+    devShells.${system} = {
+      default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          # Android
+          pinnedJDK
+          sdk
 
-        # Flutter
-        flutter dart
+          # Flutter
+          flutter
+          dart
 
-        # Code hygiene
+          # Code hygiene
           gitlint
 
-        # Flutter dependencies for linux desktop
-        atk
-        cairo
-        clang
-        cmake
-        epoxy
-        gdk-pixbuf
-        glib
-        gtk3
-        harfbuzz
-        ninja
-        pango
-        pcre
-        pcre2
-        pkg-config
-        xorg.libX11
-        xorg.xorgproto
-        clutter-gst
-        mount
-        libunwind
+          # Flutter dependencies for linux desktop
+          atk
+          cairo
+          clang
+          cmake
+          epoxy
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          ninja
+          pango
+          pcre
+          pcre2
+          pkg-config
+          xorg.libX11
+          xorg.xorgproto
+          clutter-gst
+          mount
+          libunwind
 
+          python312Packages.flet
+          python312Packages.flet-runtime
+          flet-client-flutter
 
-        python312Packages.flet
-        python312Packages.flet-runtime
-        flet-client-flutter
-        
-        libdwg
-        elfutils
-        zstd
-        orc
-        util-linux
-        libselinux
-        libsepol
-        libthai
-        libdatrie
-        python312Packages.datrie
-        xorg.libXdmcp
-        xorg.libXtst
-      ];
+          libdwg
+          elfutils
+          zstd
+          orc
+          util-linux
+          libselinux
+          libsepol
+          libthai
+          libdatrie
+          python312Packages.datrie
+          xorg.libXdmcp
+          xorg.libXtst
+        ];
 
-      # Make Flutter build on desktop
-      CPATH = "${pkgs.xorg.libX11.dev}/include:${pkgs.xorg.xorgproto}/include";
-      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ atk cairo epoxy gdk-pixbuf glib gtk3 harfbuzz pango ];
-      
-      ANDROID_HOME = "${sdk}/share/android-sdk";
-      ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
-      JAVA_HOME = pinnedJDK;
+        # Make Flutter build on desktop
+        CPATH = "${pkgs.xorg.libX11.dev}/include:${pkgs.xorg.xorgproto}/include";
+        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [atk cairo epoxy gdk-pixbuf glib gtk3 harfbuzz pango];
 
-      FLUTTER_ROOT = "${pkgs.flutter}";
-      ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/23.1.7779620/";
+        ANDROID_HOME = "${sdk}/share/android-sdk";
+        ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
+        JAVA_HOME = pinnedJDK;
 
+        FLUTTER_ROOT = "${pkgs.flutter}";
+        ANDROID_NDK_HOME = "${ANDROID_SDK_ROOT}/ndk/23.1.7779620/";
 
-      # Fix an issue with Flutter using an older version of aapt2, which does not know
-      # an used parameter.
-      GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+        # Fix an issue with Flutter using an older version of aapt2, which does not know
+        # an used parameter.
+        GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+      };
+
+      pipzone =
+        (pkgs.buildFHSUserEnv {
+          name = "pipzone";
+          targetPkgs = pkgs: (with pkgs; [
+            python312Packages.pip
+            python312Packages.virtualenv
+            python312Packages.flet
+            python312Packages.flet-runtime
+          ]);
+        }).env;
     };
-  });
+  };
 }
